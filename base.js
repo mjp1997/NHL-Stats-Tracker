f342a8a6f19481acc2ad10b5a144f3ef3611a70a
Mitch Perez <perez15@live.missouristate.edu>
2019-07-23 22:49:15 -0500

    initial changes

--- base.js
+++ base.js
@@ -4,9 +4,10 @@ let baseURL = 'http://statsapi.web.nhl.com/api/v1/teams/';
 let teamSelector = '/roster';
 
 function buttonLink() {
-	linkButton = document.getElementById(tempButton)
-	linkButton.onclick(loadAPI)
-	userInput = getElementById(searchField)
+	linkButton = document.getElementById(searchField);
+	console.log(linkButton)
+	// linkButton.onclick(loadAPI);
+	// userInput = getElementById(searchField);
 }
 function loadAPI() {
 	let url = baseURL + userInput.value() + teamSelector;
