a30b79a0ff341597b7bd2017dce73e5d8f0c6e06
Mitchell Perez <perez15@live.missouristate.edu>
2019-07-23 22:42:45 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# NHL-Stats-Tracker
+Tracks National Hockey League Statistics with an emphasis on the St. Louis Blues using the nhl statsapi
---
+++ base.js
@@ -0,0 +1,17 @@
+let stats;
+//Oskar Sundquist = 8476897
+let baseURL = 'http://statsapi.web.nhl.com/api/v1/teams/';
+let teamSelector = '/roster';
+
+function buttonLink() {
+	linkButton = document.getElementById(tempButton)
+	linkButton.onclick(loadAPI)
+	userInput = getElementById(searchField)
+}
+function loadAPI() {
+	let url = baseURL + userInput.value() + teamSelector;
+	loadJSON(url, retrieveData);
+}
+function retrieveData(data) {
+	stats = data;
+}
