5520144c8d20938e52b7f53eb2224cfdba31e3b9
Mitch Perez <perez15@live.missouristate.edu>
2019-07-25 00:39:46 -0500

    api manipulation changes

--- base.js
+++ base.js
@@ -3,14 +3,29 @@ let baseURL = 'http://statsapi.web.nhl.com/api/v1/teams/';
 let teamSelector = '/roster';
 
 function buttonLink() {
-	linkButton = document.getElementById(searchField);
-	console.log(linkButton)
-	// linkButton.onclick(loadAPI);
-	// userInput = getElementById(searchField);
+	userInput = document.getElementById('searchField').value;
+	teamsArray = ['Buffer', 'Devils', 'Islanders', 'Rangers', 'Flyers', 'Penguins',
+	'Bruins', 'Sabres', 'Canadiens', 'Senators', 'Leafs', 'Hurricanes', 'Panthers', 
+	'Lightning', 'Capitals', 'Blackhawks', 'Red Wings', 'Predators', 'Blues', 'Flames', 
+	'Avalanche', 'Oilers', 'Canucks', 'Ducks', 'Stars', 'Kings', 'Buffer', 'Sharks', 
+	'Blue Jackets', 'Wild'];
+	if(teamsArray.indexOf(userInput) == -1 ){
+		alert("Please enter a valid NHL team (then again, are the Blackhaws really valid?)")
+	}
+	teamsArray.forEach(nhlTeam => {
+		if(nhlTeam === userInput){
+			let urlParam = teamsArray.indexOf(nhlTeam)
+			urlParam = urlParam.toString()
+			loadAPI(urlParam)
+		}	
+	});
+
 }
-function loadAPI() {
-	let url = baseURL + userInput.value() + teamSelector;
-	loadJSON(url, retrieveData);
+function loadAPI(userTeam) {
+	let url = baseURL + userTeam + teamSelector;
+	console.log(userTeam)
+	console.log(url)
+	// loadJSON(url, retrieveData);
 }
 function retrieveData(data) {
 	stats = data;
