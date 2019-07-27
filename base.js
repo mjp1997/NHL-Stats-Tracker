4820fa973241c826a67a7f7f00acc58e83539b58
Mitch Perez <perez15@live.missouristate.edu>
2019-07-27 17:00:36 -0500

    css/html skeleton

--- base.js
+++ base.js
@@ -2,41 +2,49 @@ let stats;
 let baseURL = 'http://statsapi.web.nhl.com/api/v1/teams/';
 let teamSelector = '/roster';
 
-function buttonLink() {
-	userInput = document.getElementById('searchField').value;
-	teamsArray = ['Buffer', 'Devils', 'Islanders', 'Rangers', 'Flyers', 'Penguins',
-	'Bruins', 'Sabres', 'Canadiens', 'Senators', 'Leafs', 'Buffer','Hurricanes', 'Panthers', 
-	'Lightning', 'Capitals', 'Blackhawks', 'Red Wings', 'Predators', 'Blues', 'Flames', 
-	'Avalanche', 'Oilers', 'Canucks', 'Ducks', 'Stars', 'Kings', 'Buffer', 'Sharks', 
+let dropDownDiv = document.getElementById('dropDownInfo')
+teamsArray = ['---Select Option:---', 'Devils', 'Islanders', 'Rangers', 'Flyers', 'Penguins',
+	'Bruins', 'Sabres', 'Canadiens', 'Senators', 'Leafs', 'Buffer', 'Hurricanes', 'Panthers',
+	'Lightning', 'Capitals', 'Blackhawks', 'Red Wings', 'Predators', 'Blues', 'Flames',
+	'Avalanche', 'Oilers', 'Canucks', 'Ducks', 'Stars', 'Kings', 'Buffer', 'Sharks',
 	'Blue Jackets', 'Wild'];
-	if(teamsArray.indexOf(userInput) == -1 ){
+
+dropDownDiv.innerHTML = teamsArray.map(function (team) {
+	return "<option class='dropDownText'>" + team + "</option>";
+}).join("");
+function buttonLink() {
+	let userInput = document.getElementById('dropDownInfo').value
+	console.log(userInput)
+	if (teamsArray.indexOf(userInput) == -1) {
 		alert("Please enter a valid NHL team (then again, are the Blackhaws really valid?)")
 	}
 	teamsArray.forEach(nhlTeam => {
-		if(nhlTeam === userInput){
+		if (nhlTeam === userInput) {
 			let urlParam = teamsArray.indexOf(nhlTeam)
 			urlParam = urlParam.toString()
 			loadAPI(urlParam)
-		}	
+		}
 	});
 
 }
+
 function loadAPI(userTeam) {
 	let completeUrl = baseURL + userTeam + teamSelector;
 	console.log(userTeam)
 	console.log(completeUrl)
 
-	 $.ajax({
-	 	type: "GET",
-	 	url: completeUrl,
-	 	async: false,
-		 success: function(completeUrl){
-			for(i = 0; i < completeUrl['roster'].length; i++){
-			myPlayer = completeUrl['roster'][i]['person']['fullName']
-			console.log(myPlayer);
-			$('.rightContainerInfo').html(myPlayer);
-			// document.getElementById("rightContainerInfo").innerHTML = myPlayer;
+	let myDiv = document.getElementById('pasteData');
+	myDiv.innerHTML = '';
+
+	$.ajax({
+		type: "GET",
+		url: completeUrl,
+		async: false,
+		success: function (completeUrl) {
+			for (i = 0; i < completeUrl['roster'].length; i++) {
+				let myPlayer = completeUrl['roster'][i]['person']['fullName'];
+				myDiv.innerHTML += "<li class='formatResult'>" + myPlayer + "<li /> ";
+			}
 		}
-	 }
 	})
-	}
+}
