f2732b56129dcc843b2b67de9fe3bbab5acf0aca
Mitch Perez <perez15@live.missouristate.edu>
2019-08-05 18:54:58 -0500

    added a few more test cases

--- base.js
+++ base.js
@@ -1,13 +1,15 @@
 let stats;
 let baseURL = 'http://statsapi.web.nhl.com/api/v1/teams/';
+let playerURL = 'http://statsapi.web.nhl.com';
 let teamSelector = '/roster';
 
 let dropDownDiv = document.getElementById('dropDownInfo')
+//array of all 31 NHL teams
 teamsArray = ['Select An Option: ', 'NJ Devils', 'NY Islanders', 'NY Rangers', 'Philadelphia Flyers', 'Pittsburgh Penguins',
 	'Boston Bruins', 'Buffalo Sabres', 'Montreal Canadiens', 'Ottawa Senators', 'Toronto Maple Leafs', 'Carolina Hurricanes', 'Florida Panthers',
 	'Tampa Bay Lightning', 'Washington Capitals', 'Chicago Blackhawks', 'Detroit Red Wings', 'Nashville Predators', 'St. Louis Blues', 'Calgary Flames',
 	'Colorado Avalanche', ' Edmonton Oilers', 'Vancouver Canucks', ' Anaheim Ducks', 'Dallas Stars', 'Los Angeles Kings', 'San Jose Sharks',
-	'Columbus Blue Jackets', 'Minnesota Wild', 'Arizona Coyotes'];
+	'Columbus Blue Jackets', 'Minnesota Wild', 'Arizona Coyotes', 'Winnipeg Jets', 'Vegas Golden Knights'];
 
 dropDownDiv.innerHTML = teamsArray.map(function (team) {
 	return "<option class='dropDownText'>" + team + "</option>";
@@ -15,6 +17,8 @@ dropDownDiv.innerHTML = teamsArray.map(function (team) {
 function buttonLink() {
 	let userInput = document.getElementById('dropDownInfo').value
 	console.log(userInput);
+	//Editing index passed to URL in order to accomodate 
+	//for the API's odd formatting
 	teamsArray.forEach(nhlTeam => {
 		if (nhlTeam === userInput) {
 			let urlParam = teamsArray.indexOf(nhlTeam);
@@ -24,8 +28,14 @@ function buttonLink() {
 			if (urlParam > 26) {
 				urlParam = urlParam + 2;
 			}
-			if (userInput == 'Arizona Coyotes') {
+			if (userInput === 'Arizona Coyotes') {
 				urlParam = 53;
+			}
+			if (userInput === 'Winnipeg Jets') {
+				urlParam = 52;
+			}
+			if (userInput === 'Vegas Golden Knights') {
+				urlParam = 54;
 			}
 			urlParam = urlParam.toString()
 			loadAPI(urlParam)
@@ -33,14 +43,14 @@ function buttonLink() {
 	});
 
 }
-
+//composing base URL
 function loadAPI(userTeam) {
 	let completeUrl = baseURL + userTeam + teamSelector;
 	let siteUrl = baseURL + userTeam;
 	let myDiv = document.getElementById('pasteData');
 	myDiv.innerHTML = '';
 
-
+	//Get request for team website's URL
 	$.ajax({
 		type: "GET",
 		url: siteUrl,
@@ -52,7 +62,7 @@ function loadAPI(userTeam) {
 	})
 
 
-
+	//Get request for player name, position, and number
 	$.ajax({
 		type: "GET",
 		url: completeUrl,
@@ -73,4 +83,24 @@ function loadAPI(userTeam) {
 
 		}
 	})
+		/ $.ajax({
+			type: "GET",
+			url: completeUrl,
+			async: true,
+			success: function (completeUrl) {
+				for (i = 0; i < completeUrl['roster'].length; i++) {
+					let myPlayer = completeUrl['roster'][i]['person']['fullName'];
+					let playerNum = completeUrl['roster'][i]['jerseyNumber'];
+					let playerPos = completeUrl['roster'][i]['position']['abbreviation'];
+					if (playerNum == null) {
+						playerNum = '(pending)';
+						myDiv.innerHTML += "<li class='formatResult'>" + playerNum + ' ' + myPlayer + " - " + playerPos + "<li /> "
+					}
+					else {
+						myDiv.innerHTML += "<li class='formatResult'>" + '#' + playerNum + ' ' + myPlayer + " - " + playerPos + "<li /> ";
+					}
+				}
+
+			}
+		})
 }
